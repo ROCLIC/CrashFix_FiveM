4eb639b2ca01770d38169b302a134a4bd0406255
DSW-robinsonruiz <101422223+DSW-robinsonruiz@users.noreply.github.com>
2026-04-09 13:17:04 -0500

    Delete CrashFix_FiveM directory

--- CrashFix_FiveM/src/services/__init__.py
+++
@@ -1,6 +0,0 @@
-from .diagnostic_service import DiagnosticService
-from .repair_service import RepairService
-from .hardware_service import HardwareService
-from .network_service import NetworkService
-from .session_manager import SessionManager, DiagnosticSession
-__all__ = ['DiagnosticService', 'RepairService', 'HardwareService', 'NetworkService', 'SessionManager', 'DiagnosticSession']
